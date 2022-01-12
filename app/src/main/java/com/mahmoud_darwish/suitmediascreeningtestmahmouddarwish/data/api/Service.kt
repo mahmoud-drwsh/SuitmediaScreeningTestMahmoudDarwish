3b884f8df8f9739a4c961b3807c9751f94fc809d
Mahmoud Darwish <imahmwd14@gmail.com>
2022-01-12 16:41:34 +0700

    added the suspend modifier

--- app/src/main/java/com/mahmoud_darwish/suitmediascreeningtestmahmouddarwish/data/api/Service.kt
+++ app/src/main/java/com/mahmoud_darwish/suitmediascreeningtestmahmouddarwish/data/api/Service.kt
@@ -4,9 +4,10 @@ import com.mahmoud_darwish.suitmediascreeningtestmahmouddarwish.data.model.remot
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+
 interface Service {
     @GET("users")
-    fun getUsers(
+    suspend fun getUsers(
         @Query("page") page: Int = 1,
         @Query("per_page") perPage: Int = 10,
     ): ApiResponse
